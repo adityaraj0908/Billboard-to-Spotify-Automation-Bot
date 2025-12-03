8b3b0832f4f16a6257fcb5dcd5a61ba33187a2f8
Aditya Raj <adityaraj1389@gmail.com>
2025-12-03 08:01:43 +0530

    Webscraped billboard using bs4

---
+++ main.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+date = input("Which year do you want to travel to? Enter the date in this format YYYY-MM-DD:")
+header = header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"}
+billboard_url = "https://www.billboard.com/charts/hot-100/2000-08-12/"+date
+
+response = requests.get(url=billboard_url,headers=header)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=os.getenv("YOUR-CLIENT-ID"),
+        client_secret=os.getenv("YOUR-CLIENT-SECRET"),
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+
+
+user_id = sp.current_user()["id"]
+print(user_id)
+
+# Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
